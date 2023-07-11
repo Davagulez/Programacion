10793725ff362c8111db9d1394ced1a181fac4be
David Lezcano <lezcadav@gmail.com>
2023-07-11 10:56:45 -0300

    ✨ tic tac andando

--- React/guiaOficial/tic-tac-toe/src/board.jsx
+++ React/guiaOficial/tic-tac-toe/src/board.jsx
@@ -1,15 +1,29 @@
 import { Square } from "./button";
 import { useState } from "react";
+import { calculateWinner } from "./calculateWinner";
 
 export default function Board() {
+    const [xIsNext, setXIsNext] = useState(true);
     const [squares, setSquares] = useState(Array(9).fill(null));
     function handleClick(i) {
+      if (squares[i] || calculateWinner(squares)) {
+        return;
+      }
       const nextSquares = squares.slice();
-      nextSquares[i] = "X";
+      xIsNext ? nextSquares[i] = "X" : nextSquares[i] = "O";
       setSquares(nextSquares);
+      setXIsNext(!xIsNext)
+    }
+    const winner = calculateWinner(squares);
+    let status;
+    if (winner) {
+      status = "Ganador: " + winner;
+    } else {
+      status = "Proximo Jugador: " + (xIsNext ? "X" : "O");
     }
     return(
       <>
+        <div className="status">{status}</div>
         <div className="tictac__table board-row">
           <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
           <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
