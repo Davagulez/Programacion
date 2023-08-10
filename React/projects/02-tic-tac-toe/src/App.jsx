9dfcb7f0ef63ba20e036b8aa8baae5ae5578bbd3
David Lezcano <lezcadav@gmail.com>
2023-08-10 17:21:22 -0300

    feature: agregue que se guarde la partida en localStorage

--- React/projects/02-tic-tac-toe/src/App.jsx
+++ React/projects/02-tic-tac-toe/src/App.jsx
@@ -5,18 +5,27 @@ import {Square} from './components/Square.jsx'
 import {TURNS} from './constants.js'
 import {checkWinnerFrom} from './logic/board.js'
 import {WinnerModal} from './components/WinnerModal.jsx'
+import { saveGameToStorage, resetGameStorage } from './logic/storage'
 
 const board = Array(9).fill(null) 
 
 function App() {
-  const [board,setBoard] = useState(Array(9).fill(null))
-  const [turn, setTurn] = useState(TURNS.X)
+  const [board,setBoard] = useState(() => {
+    const boardFromStorage = window.localStorage.getItem('board')
+    return boardFromStorage ? JSON.parse(boardFromStorage) : Array(9).fill(null)
+  })  
+  const [turn, setTurn] = useState(() => {
+    const turnFromStorage = window.localStorage.getItem('turn')
+    return turnFromStorage ?? TURNS.X
+  })
   const [winner, setWinner] = useState(null)
 
   const resetGame = () => {
     setBoard(Array(9).fill(null))
     setTurn(TURNS.X)
     setWinner(null)
+
+    resetGameStorage()    
   }
 
   const checkEndGame = (newBoard) => {
@@ -37,6 +46,12 @@ function App() {
     //cambiar el turno
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
     setTurn(newTurn) 
+
+    //Guardar la partida
+    saveGameToStorage({
+      board: newBoard,
+      turn: newTurn
+    })
 
     //revisar si hay un ganador
     const newWinner = checkWinnerFrom(newBoard)
